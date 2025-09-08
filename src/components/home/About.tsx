0a37ae6b75bb341663435165bfc674df37895eb1
Himanshu Bijja <bijjahimanshu05@gmail.com>
2025-09-08 16:50:10 +0530

    added projects link

--- src/components/home/About.tsx
+++ src/components/home/About.tsx
@@ -1,23 +1,33 @@
 import Wrapper from "@/lib/Wrappet";
 import { Button } from "../ui/button";
 import { ArrowUpRight } from "lucide-react";
+import Link from "next/link";
 
 function About() {
   return (
     <>
-      <Wrapper title="About" >
-        <p>I'm Himanshu Bijja, a software developer based in Hyderabad, India</p> <br />
-        <p>19, building epic stuffs to explore more about <span className="text-chart-2">Python</span> and <span className="text-chart-3">JavaScript</span></p> <br />
+      <Wrapper title="About">
+        <p>
+          I'm Himanshu Bijja, a software developer based in Hyderabad, India
+        </p>{" "}
+        <br />
+        <p>
+          19, building epic stuffs to explore more about{" "}
+          <span className="text-chart-2">Python</span> and{" "}
+          <span className="text-chart-3">JavaScript</span>
+        </p>{" "}
+        <br />
         <p>
           tldr; mostly self-taught by shipping things on the internet <br />
           into tech and deep science — things that reshape us <br />
           fascinated by space,s science, and ideas that expand the mind
         </p>
-
-        <Button className="mt-8 flex flex-row gap-2" >
-          Projects
-          <ArrowUpRight />
-        </Button>
+        <Link href="/projects">
+          <Button className="mt-8 flex flex-row gap-2">
+            Projects
+            <ArrowUpRight />
+          </Button>
+        </Link>
       </Wrapper>
     </>
   );
